5c60df34625b5a2883ba72284949ed884735fc67
Narayan Aryal <aryal.coding@gmail.com>
2023-09-26 23:09:26 +1000

    code completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { client } from './lib/sanity';
 import { Post } from './lib/interface';
+import Link from 'next/link';
 
 
 const getData = async() => {
@@ -32,6 +33,18 @@ const IndexPage =async () => {
           
           {new Date(post._createdAt).toISOString().split("T")[0]} </p>
       </div>
+
+      <Link href={`/post/${post.slug.current}`} prefetch className="space-y-3 xl:col-span-3">
+<div>
+  <h3 className='text-2xl font-bold leading-8 tracking-light text-gray-900 dark:text-gray-100'>
+{post.title}
+  </h3>
+</div>
+<p className='prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2'>
+{post.overview}
+</p>
+
+      </Link>
 
     </article>
 
