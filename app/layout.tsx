3a1e636c8681b2578a143586ac649a553198b454
Narayan Aryal <aryal.coding@gmail.com>
2024-03-09 12:41:18 +1100

    new blog added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Navbar } from './components/Navbar'
-import { Providers } from './components/Providers'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Navbar } from "./components/Navbar";
+import { Providers } from "./components/Providers";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Emprcode blog',
-  description: 'Unlocking the Code: Where Software Meets Stories',
-}
+  title: "Emprcode blog",
+  description: "Unlocking the Code: Where Software Meets Stories",
+};
+//this is the main code
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      
-      <body className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50` }>
+      <body
+        className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}
+      >
         <Providers>
-        <Navbar/>
-        <main className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8'>
-
-        
-        {children}
-        </main>
+          <Navbar />
+          <main className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
         </Providers>
-        
-        
-        </body>
+      </body>
     </html>
-  )
+  );
 }
